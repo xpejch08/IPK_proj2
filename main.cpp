5ed2ef15bf634eaa5b9ff5bea3eaafc7bfe8675c
xpejch08 <pejcharstepan@gmail.com>
2023-04-16 20:54:46 +0200

    lpcap linker

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
-#include <pcap.h>
 #include <iomanip>
 #include <signal.h>
 #include <netinet/ip.h>
@@ -12,6 +11,10 @@
 #include <netinet/ether.h>
 #include <netinet/ip6.h>
 #include <chrono>
+#include <pcap.h>
+#include <sstream>
+
+pcap_t *snifedDeviceGlobal;
 
 
 class packetSniffer {
@@ -43,17 +46,16 @@ class packetSniffer {
         }
     };
     void activeInterface(std::string interfaceArgument, pcap_if_t *allInterfaces){
-        pcap_if_t *interface;
-        int i = 0;
-        for(interface = allInterfaces; interface; interface = interface->next){
-            if(interfaceArgument == interface->name){
-                std::cout << "Interface: " << interface->name << std::endl;
-                return;
+        while (allInterfaces->next != nullptr) {
+            if (allInterfaces->name == interfaceArgument) {
+                interfaceExists = true;
             }
-            i++;
+            allInterfaces = allInterfaces->next;
         }
-        std::cout << "Interface not found" << std::endl;
-        exit(1);
+        if(!interfaceExists) {
+            pcap_freealldevs(allInterfaces);
+            std::cerr << "Interface: " << interfaceArgument << " does not exist" << std::endl;
+        }
     };
 
     void pcapFilter(){
@@ -73,7 +75,7 @@ class packetSniffer {
             }
         }
         if (icmp4) {
-            pcFilter = "(icmp4) or";
+            pcFilter = "(icmp) or";
         }
         if (icmp6) {
             pcFilter = "(icmp6) or";
@@ -82,10 +84,10 @@ class packetSniffer {
             pcFilter = "(igmp) or";
         }
         if (mld) {
-            pcFilter = "(mld) or";
+            pcFilter += "(icmp6 and ip6[40] == 143) or";
         }
         if (ndp) {
-            pcFilter = "(ndp) or";
+            pcFilter += "(icmp6 and (ip6[40] == 135 or ip6[40] == 136)) or";
         }
         if (arp) {
             pcFilter = "(arp) or";
@@ -101,22 +103,80 @@ class packetSniffer {
                 << std::setfill('0') << std::setw(3) << millis.count() << "Z";
         printf("timestamp: %s \n", stream.str().c_str());
     }
-    void printMacAddress(auto filter){
-        printf("src MAC: %02X:%02X:%02X:%02X:%02X:%02X",
+    void printMacAndFrameLen(const struct ether_header *filter, unsigned int frameLen){
+        printf("src MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
                filter->ether_shost[0], filter->ether_shost[1],
                filter->ether_shost[2], filter->ether_shost[3],
                filter->ether_shost[4], filter->ether_shost[5]);
 
-        printf("dst MAC: %02X:%02X:%02X:%02X:%02X:%02X",
+        printf("dst MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
                filter->ether_dhost[0], filter->ether_dhost[1],
                filter->ether_dhost[2], filter->ether_dhost[3],
                filter->ether_dhost[4], filter->ether_dhost[5]);
+
+        printf("frame length: %d\n", frameLen);
+
+    }
+    void srcIP(struct ip *ip){
+        printf("src IP: %s\n", inet_ntoa(ip->ip_src));
+        printf("dst IP: %s\n", inet_ntoa(ip->ip_dst));
+    }
+
+    void printIpOfIpv6(const struct in6_addr *src, const struct in6_addr *dest){
+        printf("src IP: %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x\n",
+                (int)src->s6_addr[0], (int)src->s6_addr[1],
+                (int)src->s6_addr[2], (int)src->s6_addr[3],
+                (int)src->s6_addr[4], (int)src->s6_addr[5],
+                (int)src->s6_addr[6], (int)src->s6_addr[7],
+                (int)src->s6_addr[8], (int)src->s6_addr[9],
+                (int)src->s6_addr[10], (int)src->s6_addr[11],
+                (int)src->s6_addr[12], (int)src->s6_addr[13],
+                (int)src->s6_addr[14], (int)src->s6_addr[15]);
+        printf("dst IP: %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x\n",
+                (int)dest->s6_addr[0], (int)dest->s6_addr[1],
+                (int)dest->s6_addr[2], (int)dest->s6_addr[3],
+                (int)dest->s6_addr[4], (int)dest->s6_addr[5],
+                (int)dest->s6_addr[6], (int)dest->s6_addr[7],
+                (int)dest->s6_addr[8], (int)dest->s6_addr[9],
+                (int)dest->s6_addr[10], (int)dest->s6_addr[11],
+                (int)dest->s6_addr[12], (int)dest->s6_addr[13],
+                (int)dest->s6_addr[14], (int)dest->s6_addr[15]);
+    }
+
+    void printByteOffset(const void *data, int byte_offset_size) {
+        char byteOffsetBuff [20]= {'\0'};
+        int i = 0;
+        unsigned char *byte_offset_hexa = (unsigned char *)data;
+        for (i; i < byte_offset_size; i++) {
+            if (i%16 == 0) {
+                if (i != 0) {
+                    printf("  %s\n", byteOffsetBuff);
+                }
+                printf("0x%04", i);
+            }
+            printf(" %02x", byte_offset_hexa[i]);
+            if ((byte_offset_hexa[i] < 0x20) || (byte_offset_hexa[i] > 0x7e)) {
+                byteOffsetBuff[i%16] = '.';
+            } else {
+                byteOffsetBuff[i%16] = byte_offset_hexa[i];
+            }
+            byteOffsetBuff[(i%16)+1] = '\0';
+        }
+        while ((i%16) != 0) {
+            printf("   ");
+            i++;
+        }
+        printf("  %s\n", byteOffsetBuff);
+    }
+
+    void printSrcDstArp(char *src, char *dst){
+        printf("src IP: %s\n", src);
+        printf("src IP: %s\n", dst);
     }
 
     static void packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
         snifferForPacket->timeStamp();
         auto size = header->len;
-        struct mac_filter *macFilter = (struct mac_filter*)packet;
         const struct iphdr *ipHeader = (struct iphdr*)(packet + sizeof(struct ether_header));
         const struct ether_header *ethernetHeader = (struct ether_header*)packet;
         auto *ipH = (struct ip*)(packet + sizeof(struct ether_header));
@@ -125,53 +185,77 @@ class packetSniffer {
         const struct icmp *icmpHeader = (struct icmp*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
         const struct ip6_hdr *ip6Header = (struct ip6_hdr*)(packet + sizeof(struct ether_header));
         const struct ip4_hdr *ip4Header = (struct ip4_hdr*)(packet + sizeof(struct ether_header));
-        auto *arpHeader = (struct ether_arp*)(packet + sizeof(struct ether_header));
+        struct ip *arpHeader = (struct ip*)(packet + sizeof(struct ether_header));
+        u_short len = (ipHeader->ihl) * 4;
 
         if(ethernetHeader->ether_type == htons(ETHERTYPE_IP)){
             //icmp IPV4
             if(ipHeader->protocol == 1){
-                snifferForPacket->printMacAddress(ethernetHeader);
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->srcIP(arpHeader);
 
             }
             //igmp ipv4
             else if(ipHeader->protocol == 2){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->srcIP(arpHeader);
             }
             //tcp ipv4
             else if(ipHeader->protocol == 6){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->srcIP(arpHeader);
+                printf("\nsrc port: %d\n", ntohs(tcpHeader->source));
+                printf("\ndst port: %d\n", ntohs(tcpHeader->dest));
             }
             //udp ipv4
             else if(ipHeader->protocol == 17){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->srcIP(arpHeader);
+                printf("\nsrc port: %d\n", ntohs(udpHeader->source));
+                printf("\ndst port: %d\n", ntohs(udpHeader->dest));
             }
         }
         else if(ethernetHeader->ether_type == htons(ETHERTYPE_IPV6)){
             //icmp ipv6
             if(ip6Header->ip6_ctlun.ip6_un1.ip6_un1_nxt == 58){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->printIpOfIpv6(&ip6Header->ip6_src, &ip6Header->ip6_dst);
             }
             //mld ipv6
             else if(ip6Header->ip6_ctlun.ip6_un1.ip6_un1_nxt == 58){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->printIpOfIpv6(&ip6Header->ip6_src, &ip6Header->ip6_dst);
             }
             //tcp ipv6
             else if(ip6Header->ip6_ctlun.ip6_un1.ip6_un1_nxt == 6){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->printIpOfIpv6(&ip6Header->ip6_src, &ip6Header->ip6_dst);
+                printf("\nsrc port: %d\n", ntohs(tcpHeader->source));
+                printf("dst port: %d\n", ntohs(tcpHeader->dest));
+
             }
             //udp ipv6
             else if(ip6Header->ip6_ctlun.ip6_un1.ip6_un1_nxt == 17){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+                snifferForPacket->printIpOfIpv6(&ip6Header->ip6_src, &ip6Header->ip6_dst);
+                printf("\nsrc port: %d\n", ntohs(udpHeader->source));
+                printf("dst port: %d\n", ntohs(udpHeader->dest));
             }
             //ndp ipv6
             else if(ip6Header->ip6_ctlun.ip6_un1.ip6_un1_nxt == 58){
-                printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+                snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
             }
         }
         else if(ethernetHeader->ether_type == htons(ETHERTYPE_ARP)){
-            printf("src MAC: %s \n", ether_ntoa((struct ether_addr*)ethernetHeader->ether_shost));
+            struct ether_arp *arp = (struct ether_arp *)(packet + sizeof(struct ether_header) + len);
+            char IP[16], IP2[16];
+            inet_ntop(AF_INET, &(arp->arp_spa), IP, sizeof(IP));
+            inet_ntop(AF_INET, &(arp->arp_tpa), IP2, sizeof(IP2));
+            snifferForPacket->printMacAndFrameLen(ethernetHeader, size);
+            snifferForPacket->printSrcDstArp(IP, IP2);
         }
-
+        std::cout << std::endl;
+        snifferForPacket->printByteOffset(packet, int(size));
 
     };
     // Other class members go here
@@ -213,14 +297,21 @@ class packetSniffer {
                 interface = true;
                 try {
                     interfaceArgument = std::string(argv[i+1]);
+                    i++;
                 }
                 catch (std::exception const &e) {
                     printInterfaces(allInterfaces);
                 }
-                activeInterface(interfaceArgument, allInterfaces);
+                if(interfaceArgument == ""){
+                    printInterfaces(allInterfaces);
+                    exit(0);
+                }
+                else{
+                    activeInterface(interfaceArgument, allInterfaces);
+                }
             }
             else if(argument == "--arp"){
-
+                arp = true;
             }
             else if(argument == "--icmp4"){
                 icmp4 = true;
@@ -232,10 +323,10 @@ class packetSniffer {
                 ndp = true;
             }
             else if(argument == "--igmp"){
-
+                igmp = true;
             }
-            else if(argument == "mld"){
-
+            else if(argument == "--mld"){
+                mld = true;
             }
             else if(argument == "-n"){
                 n = true;
@@ -252,18 +343,22 @@ class packetSniffer {
                 }else{
                     packetCount = packetCountTmp;
                 }
+                i++;
             }
             else{
                 std::cout << "Error: invalid argument" << std::endl;
                 exit(1);
             }
         }
-        if(!udp && !tcp && !icmp4 && !icmp6 && !igmp){
+        if(!udp && !tcp && !icmp4 && !icmp6 && !igmp && !ndp && !arp && !mld){
             udp = true;
             tcp = true;
             icmp4 = true;
             icmp6 = true;
             igmp = true;
+            ndp = true;
+            arp = true;
+            mld = true;
         }
         if (!interface) {
             printInterfaces(allInterfaces);
@@ -287,6 +382,7 @@ class packetSniffer {
     bool p = false;
     bool udp = false;
     bool interface = false;
+    bool interfaceExists = false;
     bool tcp = false;
     bool help = false;
     int packetCount = 0;
@@ -303,7 +399,11 @@ class packetSniffer {
     private:
     static packetSniffer * snifferForPacket;
 };
-
+void signalHandler(int signum){
+    std::cout << "Interrupt signal (" << signum << ") received." << std::endl;
+    pcap_close(snifedDeviceGlobal);
+    exit(signum);
+}
 
 packetSniffer * packetSniffer::snifferForPacket = nullptr;
 
@@ -312,19 +412,21 @@ int main(int argc, char *argv[]){
     sniffer.checkArguments(argc, argv);
     sniffer.pcapFilter();
     packetSniffer::setPacketObject(&sniffer);
+    signal(SIGINT, signalHandler);
 
 
-    if (pcap_lookupnet(sniffer.interfaceArgument.c_str(), &sniffer.ipOfSniffed, &sniffer.netmaskOfSniffed, sniffer.errbuf) == -1) {
+    if (pcap_lookupnet(sniffer.interfaceArgument.c_str(), &sniffer.ipOfSniffed, &sniffer.netmaskOfSniffed, sniffer.errbuf) == PCAP_ERROR) {
         fprintf(stderr, "Can't get netmask for device %s. ErrorBuffer: %s", sniffer.interfaceArgument.c_str(),
                 sniffer.errbuf);
         return 1;
     }
-    if((sniffer.sniffedDevice = pcap_open_live(sniffer.interfaceArgument.c_str(), BUFSIZ, 1, 1000, sniffer.errbuf)) == nullptr){
+    if((sniffer.sniffedDevice = pcap_open_live(sniffer.interfaceArgument.c_str(), BUFSIZ, 1, 100, sniffer.errbuf)) == nullptr){
         pcap_close(sniffer.sniffedDevice);
         fprintf(stderr, "Can't open device %s. ErrorBuffer: %s", sniffer.interfaceArgument.c_str(),
                 sniffer.errbuf);
         return 1;
     }
+    snifedDeviceGlobal = sniffer.sniffedDevice;
     if (pcap_datalink(sniffer.sniffedDevice) != DLT_EN10MB) {
         fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported", sniffer.interfaceArgument.c_str());
         return 1;
@@ -346,6 +448,7 @@ int main(int argc, char *argv[]){
         pcap_close(sniffer.sniffedDevice);
         return 1;
     }
+    pcap_close(sniffer.sniffedDevice);
     return 0;
 
 }
